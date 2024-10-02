682ff6ef3847643251b19ade1edbfa363de73277
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2024-10-02 18:34:02 -0400

    Add file reading

--- Vsim.c
+++ Vsim.c
@@ -1,7 +1,44 @@
-#include <stdio.h>
+// On my honor, I have neither given nor recieved ay unauthroized aid on this assignment
 
-int main(int, char**) {
-    int i = 10;
-    printf("hello world %d", i + 10);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// argc is # of arguments including program execution
+// argv is the array of strings of every execution
+int main(int argc, char* argv[]) {
+
+  if (argc != 2) { // check to see if correct amnt of arguments entered
+    printf("ERROR: Usage: %s filename\n", argv[0]);
     return 0;
+  }
+
+  // --- Load text file ---
+  FILE* fp;
+  fp = fopen(argv[1], "r");
+
+  // Error handle file
+  if (fp == NULL) {
+    printf("ERROR: %s could not be opened for reading.\n", argv[1]);
+    return 0;
+  }
+
+  // Init the file reader
+  char line[35]; // 32 bit word + \n\r + \0
+
+  // Itterate over every line in the file
+  while (fgets(line, 1024, fp) != NULL) {
+    printf("%s", line);
+  }
+
+  fclose(fp);
+  return 0;
 }
+
+// Define Registers
+const int x0 = 0;
+int x1, x2, x3, x4, x5, x6, x7, x8         = 0;
+int x9, x10, x11, x12, x13, x14, x15, x16  = 0;
+int x17, x18, x19, x20, x21, x22, x23, x24 = 0;
+int x25, x26, x27, x28, x29, x30, x31 = 0;
+int pc = 256; // assignment defines input as starting at address 256
