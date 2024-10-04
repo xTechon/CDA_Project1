f1d79389cf38d8a896c44399d29479998c6d4620
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2024-10-04 14:40:02 -0400

    Move parse file into function

--- Vsim.c
+++ Vsim.c
@@ -1,10 +1,38 @@
 // On my honor, I have neither given nor recieved ay unauthroized aid on this assignment
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/queue.h>
+
+// Parse the input file
+void parseFile(FILE* fp);
+
+// argc is # of arguments including program execution
+// argv is the array of strings of every argument including execution
+int main(int argc, char* argv[]) {
+
+  if (argc != 2) { // check to see if correct amnt of arguments entered
+    printf("ERROR: Usage: %s filename\n", argv[0]);
+    return 0;
+  }
+
+  // --- Load text file ---
+  FILE* fp;
+  fp = fopen(argv[1], "r");
+
+  // --- Error handle file ---
+  if (fp == NULL) {
+    printf("ERROR: %s could not be opened for reading.\n", argv[1]);
+    return 0;
+  }
+
+  // --- Parse file and load into "memory" ---
+  parseFile(fp);
+
+  fclose(fp);
+  return 0;
+}
 
 // Define type to point to functions
 typedef void (*func_type)(void);
@@ -66,51 +94,94 @@ func_type c3[6]       = {addi, andi, ori, sll, sra, lw};
 func_type c4[2]       = {jal, br};
 func_type* opcodes[4] = {c4, c2, c3, c1};
 
-// argc is # of arguments including program execution
-// argv is the array of strings of every execution
-int main(int argc, char* argv[]) {
+typedef struct cat_1
+{
+  int category;
+  int opcode;
+  int imm1;
+  int rs1;
+  int rs2;
+  int imm2;
+} cat_1;
 
-  if (argc != 2) { // check to see if correct amnt of arguments entered
-    printf("ERROR: Usage: %s filename\n", argv[0]);
-    return 0;
-  }
+typedef struct cat_2
+{
+  int category;
+  int opcode;
+  int rd;
+  int rs1;
+  int rs2;
+  int func7;
+} cat_2;
 
-  // --- Load text file ---
-  FILE* fp;
-  fp = fopen(argv[1], "r");
+typedef struct cat_3
+{
+  int category;
+  int opcode;
+  int rd;
+  int rs1;
+  int imm1;
+} cat_3;
 
-  // Error handle file
-  if (fp == NULL) {
-    printf("ERROR: %s could not be opened for reading.\n", argv[1]);
-    return 0;
-  }
+typedef struct cat_4
+{
+  int category;
+  int opcode;
+  int rd;
+  int imm1;
+} cat_4;
 
+void parseFile(FILE* fp) {
   // Init the file reader
   char line[35]; // 32 bit word + \n\r + \0
   char op[6];
 
   bool endFlag = false;
+
   // Itterate over every line in the file
   while (fgets(line, 35, fp) != NULL) {
 
-    if (endFlag == true) continue;
+    if (endFlag == true) {
+      int num = (int) strtol(line, NULL, 2);
+      printf("number = %d\n", num);
+      continue;
+    }
 
     // Get the category code
     char catStr[3] = {line[30], line[31], '\0'};
     short category = (short) strtol(catStr, NULL, 2);
 
+    // Process opcodes
     strncpy(op, &line[25], 5);              // Get the opcode
     op[5]      = '\0';                      // Terminate Manually
     int opcode = (int) strtol(op, NULL, 2); // convert to decimal
 
-    // 11111
-    if (opcode == 31) { opcode = 1; }
+    // 11111, start reading integer values
+    if (opcode == 31) {
+      opcode  = 1;
+      endFlag = true;
+    }
     printf("cat: %d, opcode: %d ", category + 1, opcode);
     opcodes[category][opcode]();
 
+    // get arguments based on category
+
+    // func_type* opcodes[4] = {c4, c2, c3, c1};
+    switch (category) {
+    // cat 4
+    case 0:
+      break;
+    // cat 2
+    case 1:
+      break;
+    // cat 3
+    case 2:
+      break;
+    // cat 1
+    case 3:
+      break;
+    }
+
     // printf("%s\n", op);
   }
-
-  fclose(fp);
-  return 0;
 }
