489748996e973b3ff5e94fdbc5f667df879e59cf
xTechon <DanielHiromoto@outlook.com>
2024-10-04 18:15:28 -0400

    Add Cat 4 instruction parsing

--- Vsim.c
+++ Vsim.c
@@ -102,6 +102,7 @@ typedef struct cat_1
   int imm1;
   int rs1;
   int rs2;
+  // int func3
   int imm2;
 } cat_1;
 
@@ -110,9 +111,10 @@ typedef struct cat_2
   int category;
   int opcode;
   int rd;
+  // int func3;
   int rs1;
   int rs2;
-  int func7;
+  // int func7;
 } cat_2;
 
 typedef struct cat_3
@@ -120,6 +122,7 @@ typedef struct cat_3
   int category;
   int opcode;
   int rd;
+  // func3
   int rs1;
   int imm1;
 } cat_3;
@@ -135,16 +138,16 @@ typedef struct cat_4
 // create a queue to place lines of the file
 typedef void* block;
 
-struct entry
+typedef struct entry
 {
   block data;               // store the instruction
   char* line;               // store the original binary
   STAILQ_ENTRY(entry) next; // link to next portion of memory
-};
+} entry;
 
-STAILQ_HEAD(stailhead, entry);
+STAILQ_HEAD(stailhead, entry); // create the type for head of the queue
 
-struct stailhead MEMORY;
+struct stailhead memqueue; // queue to push parsed instructions
 
 void parseFile(FILE* fp) {
 
@@ -152,11 +155,16 @@ void parseFile(FILE* fp) {
   char line[35]; // 32 bit word + \n\r + \0
   char op[6];
 
+  // flag for the end of a program
   bool endFlag = false;
+
+  // init the queue
+  STAILQ_INIT(&memqueue);
 
   // Itterate over every line in the file
   while (fgets(line, 35, fp) != NULL) {
 
+    // for reading signed integers after break
     if (endFlag == true) {
       int num = (int) strtol(line, NULL, 2);
       printf("number = %d\n", num);
@@ -182,24 +190,49 @@ void parseFile(FILE* fp) {
 
     // get arguments based on category
 
-    void* instruction;
-    // func_type* opcodes[4] = {c4, c2, c3, c1};
+    // create copy of line
+    char cpy[33];
+    strncpy(cpy, line, 32);
+    cpy[32] = '\0';
+
+    // create instructions for each category and place in queue
     switch (category) {
     // cat 4
     case 0:
-      instruction = malloc(sizeof(cat_4));
+      // Extract imm from text
+      char im[20];
+      strncpy(im, line, 19);
+      im[19]  = '\0';
+      // convert to decimal
+      int imm = (int) strtol(im, NULL, 2);
+
+      // Extract rd from text (len 6 @ 20)
+      char reg[7];
+      strncpy(reg, &line[20], 6);
+      reg[6] = '\0';
+      int rd = (int) strtol(reg, NULL, 2);
+
+      // create instruction
+      cat_4 instruction = {.category = category, .opcode = opcode, .imm1 = imm, .rd = rd};
+      // create queue item
+      entry* item       = malloc(sizeof(entry));
+      item->data        = &instruction;
+      item->line        = cpy;
+
+      // place item on queue
+      STAILQ_INSERT_HEAD(&memqueue, item, next);
       break;
     // cat 2
     case 1:
-      instruction = malloc(sizeof(cat_2));
+      // instruction = malloc(sizeof(cat_2));
       break;
     // cat 3
     case 2:
-      instruction = malloc(sizeof(cat_3));
+      // instruction = malloc(sizeof(cat_3));
       break;
     // cat 1
     case 3:
-      instruction = malloc(sizeof(cat_1));
+      // instruction = malloc(sizeof(cat_1));
       break;
     }
 
